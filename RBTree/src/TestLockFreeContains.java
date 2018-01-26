d50a1e55da510309628f80834539ad362d025d21
Abhijeet <abhijeet.tawar007@gmail.com>
2018-01-26 17:52:41 -0500

    5510 submission

---
+++ RBTree/src/TestLockFreeContains.java
@@ -0,0 +1,47 @@
+
+import LockFreeContains.*;
+public class TestLockFreeContains 
+{
+	static int numOfThreads = 6;
+	static int numOfTasks = 1000;
+	static String TREE_TYPE = "LockFreeRBTree";
+	
+	public static void main(String[] args) throws InterruptedException, InstantiationException, IllegalAccessException, ClassNotFoundException 
+	{
+		if(args.length != 3)
+		{
+			System.out.println("wrong arguments...");
+			System.out.println("Use: java TestLockFreeContains <Name of the tree> <number of threads> <number of tasks>");
+			return;
+		}
+
+		TREE_TYPE = args[0];
+		numOfThreads = Integer.parseInt(args[1]);
+		numOfTasks = Integer.parseInt(args[2]);
+
+		RunThreadNew[] threads = new RunThreadNew[numOfThreads];
+		RBTree rbTreeObj = (RBTree)Class.forName("LockFreeContains." + TREE_TYPE).newInstance();
+		
+		for(int i=0; i<numOfThreads; i++)
+		{
+			threads[i] = new RunThreadNew(rbTreeObj, numOfThreads, numOfTasks);
+		}
+		long startTime = System.nanoTime();
+		for(int i=0; i<numOfThreads; i++)
+		{
+			threads[i].start();
+		}
+		
+		for(int i=0; i<numOfThreads; i++)
+		{
+			threads[i].join();
+		}
+		
+		long endTime = System.nanoTime();
+		
+		rbTreeObj.printME();
+		double msTime = (double)(endTime- startTime)/1000000;
+
+		System.out.println("Total time " + msTime + " ms");
+	}
+}
